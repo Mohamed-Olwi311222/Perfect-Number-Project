f515b7fccb6a59d7e5092a1f264951542b1f7a4f
AceMido <111067383+AceMido@users.noreply.github.com>
2022-12-15 14:55:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Perfect_numbers_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number to start counting from: ");
+            int num1=int.Parse(Console.ReadLine());
+            
+            Console.Write("Enter the number to stop counting to: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            
+            for (int c=num1;c<=num2;c++)
+            {
+                int sum = 0;
+                for (int d=1;d<c;d++)
+                {
+                    if (c % d == 0)
+                       sum += d; 
+                    else
+                        continue;
+                }
+                if (sum == c)
+                {
+                    Console.WriteLine(c);
+                }
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
